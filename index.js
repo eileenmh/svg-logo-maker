0206b4e1f6d572315d8a015d1cb66c205545a568
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-06-04 22:07:45 -0400

    set up inquirer prompts

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
 const fse = require("fs-extra");
+const Shapes = require("./lib/shapes");
+
+console.log(typeof Shapes.Circle);
+
 const inquirer = require("inquirer");
 
-inquirer.prompt();
+function init() {
+  console.log("Answer the following prompts to create a SVG Logo!");
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "characters",
+        message:
+          "Enter up to three characters. The characters will be placed in the center of the logo.",
+      },
+      {
+        type: "input",
+        name: "color",
+        message: "Enter the color you want the logo to be.",
+      },
+      {
+        type: "choices",
+        name: "shape",
+        message: "Select the shape you want the logo to be.",
+        choices: ["circle", "triangle", "square"],
+      },
+    ])
+    .then();
+}
+
+init();
